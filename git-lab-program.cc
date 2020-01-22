e69d627259e82819f08b878a0f511a8de946f8a3
Mihajlo-Chonev <mc291218@ohio.edu>
2020-01-22 18:58:30 -0500

    what the??

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Mihajlo Chonev
+ *        Date: 01.22.2020
+ * Description: github duh
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "\n\nALOHA" << '!' << "\nStop laughling" << '!' << '?' << '!' << '?' << "\nHalo git!!!!\n" << endl;
+    return 0;
+}// main
